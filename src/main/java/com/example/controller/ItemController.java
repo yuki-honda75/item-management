3811df36c1018785a188667959af7f752667b11d
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-03-31 16:25:54 +0900

    ItemControllerのリスト表示のメソッドの詳細を記述

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -2,8 +2,17 @@ package com.example.controller;
 
 
 
+import com.example.domain.Item;
+import com.example.service.ItemService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 /**
  * 
@@ -13,8 +22,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/item")
 public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
     @RequestMapping("/list")
-    public String showList() {
+    public String showList(@PageableDefault(page = 0, size = 20) Pageable pageable, Model model) {
+        Page<Item> page = itemService.showList(pageable);
+
+        model.addAttribute("page", page);
+        model.addAttribute("itemList", page.getContent());
+
         return "list";
     }
     
--- src/main/java/com/example/service/ItemService.java
+++ src/main/java/com/example/service/ItemService.java
@@ -1,5 +1,11 @@
 package com.example.service;
 
+import com.example.domain.Item;
+import com.example.repository.ItemRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,5 +15,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ItemService {
+	@Autowired
+	private ItemRepository itemRepository;
 
+	public Page<Item> showList(Pageable pageable) {
+		return itemRepository.findAll(pageable);
+	}
 }
