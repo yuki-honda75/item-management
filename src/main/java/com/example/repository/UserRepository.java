0a51861098afcd71a54cc16fbe7ccf1c29fd4fd2
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-04-04 16:09:22 +0900

    usersのインサート処理追加

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.example.repository;
 
+import com.example.domain.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,5 +15,20 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class UserRepository {
+    @Autowired
+    private NamedParameterJdbcTemplate template;
 
+    /**
+     * ユーザーを登録します
+     * 
+     * @param user
+     */
+    public void insert(User user) {
+        String sql = "INSERT INTO users (name, password, authority) VALUES (:name, :password, 0)";
+        SqlParameterSource param = 
+            new MapSqlParameterSource()
+            .addValue("name", user.getName()).addValue("password", user.getPassword());
+
+        template.update(sql, param);
+    }
 }
