712a55ce0317ea5a0730d7cd562a0ab368713a96
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-04-04 18:01:54 +0900

    名前からユーザを検索する処理を追加

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.example.repository;
 
+import java.util.List;
+
 import com.example.domain.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -18,6 +22,8 @@ public class UserRepository {
     @Autowired
     private NamedParameterJdbcTemplate template;
 
+    private static final RowMapper<User> USER_ROW_MAPPER = new BeanPropertyRowMapper<>(User.class);
+
     /**
      * ユーザーを登録します
      * 
@@ -30,5 +36,16 @@ public class UserRepository {
             .addValue("name", user.getName()).addValue("password", user.getPassword());
 
         template.update(sql, param);
+    }
+
+    public User findByName(String name) {
+        String sql = "SELECT id, name, password, authority FROM users WHERE name=:name";
+        SqlParameterSource param = new MapSqlParameterSource().addValue("name", name);
+        List<User> userList = template.query(sql, param, USER_ROW_MAPPER);
+        if (userList.isEmpty()) {
+            return null;
+        }
+        
+        return userList.get(0);
     }
 }
