0848346f25f09255cd94f5418268a70a86ce1108
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-04-05 13:57:55 +0900

    検索用フォームクラス作成

--- src/main/java/com/example/form/ItemSearchForm.java
+++ src/main/java/com/example/form/ItemSearchForm.java
@@ -5,5 +5,35 @@ package com.example.form;
  *
  */
 public class ItemSearchForm {
+    /** アイテム名 */
+    private String name;
+    /** カテゴリ（小カテゴリID） */
+    private String category;
+    /** ブランド */
+    private String brand;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemSearchCondition [brand=" + brand + ", category=" + category + ", name=" + name + "]";
+    }
 
 }
